74ef4902bd286c5606d6b6a284af8f427b72b92d
Zamil Khan <143739180+Zamil007@users.noreply.github.com>
2023-12-19 12:18:54 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !== str2.length) return false;
+  // now creating a array for char to and initial having zeros; 
 
-}
+  const charMap = Array(26).fill(0);
+
+  for(let i = 0; i > str1.length; i++){
+    charMap[str1.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    charMap[str2.chatCodeAt(i) - 'a'.charCodeAt(0)]--;
+  }
+  return !charMap.includes(1);
 
 module.exports = isAnagram;
